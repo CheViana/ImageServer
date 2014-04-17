a7779af51f90f3cc730231dcec2f4e5ecc2788f0
CheViana <raduga_jenya@yahoo.com>
2014-04-17 18:45:45 +0300

    initial simple project

---
+++ ImageCropTry/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ImageCropTry
+{
+    class Program
+    {
+        
+        static void TestBigImagePartDrawing()
+        {
+            using (var absentRectangleImage = (Bitmap)Bitmap.FromFile(@"C:\photoes\acid-trip-tiff.tif"))
+            {
+                using (var currentTile = new Bitmap(256, 256))
+                {
+                    currentTile.SetResolution(absentRectangleImage.HorizontalResolution, absentRectangleImage.VerticalResolution);
+
+                    using (var currentTileGraphics = Graphics.FromImage(currentTile))
+                    {
+                        currentTileGraphics.Clear(Color.Black);
+                        var absentRectangleArea = new Rectangle(300, 300, 256, 256);
+                        currentTileGraphics.DrawImage(absentRectangleImage, 0, 0, absentRectangleArea, GraphicsUnit.Pixel);
+                    }
+
+                    currentTile.Save(@"C:\photoes\acid-trip-tile.bmp");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            TestBigImagePartDrawing();
+        }
+    }
+}
---
+++ ImageServer/Controllers/ImageController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+using System.IO;
+
+namespace ImageServer.Controllers
+{
+    public class ImageController : Controller
+    {
+        public ActionResult Index(int x,int y,int w,int h,int percentage=100)
+        {
+            using (var absentRectangleImage = (Bitmap)Bitmap.FromFile(@"D:\wallpapers\july-10-photographydock-nocal-1920x1200.tif"))
+            {
+                var imageHeigth = absentRectangleImage.Height;
+                var imageWidth = absentRectangleImage.Width;
+                var finalWidth = w;
+                var finalHeigth = h;
+                if (imageHeigth < y + h)
+                {
+                    finalHeigth = imageHeigth - y;
+                }
+                if (imageWidth < x + w)
+                {
+                    finalWidth = imageWidth - x;
+                }
+                using (var currentTile = new Bitmap(finalWidth, finalHeigth))
+                {
+                    currentTile.SetResolution(absentRectangleImage.HorizontalResolution, absentRectangleImage.VerticalResolution);
+
+                    using (var currentTileGraphics = Graphics.FromImage(currentTile))
+                    {
+                        currentTileGraphics.Clear(Color.Black);
+                        var absentRectangleArea = new Rectangle(x, y, w, h);
+                        currentTileGraphics.DrawImage(absentRectangleImage, 0, 0, absentRectangleArea, GraphicsUnit.Pixel);
+                    }
+
+                    using (var memStream = new MemoryStream())
+                    {
+                        currentTile.Save(memStream, ImageFormat.Jpeg);
+                        var bytes = memStream.ToArray();
+                        return base.File(bytes,"image/jpeg");
+                    }
+                }
+            }           
+        }
+
+        public ActionResult Full(string format = "jpeg") 
+        {
+            using (var absentRectangleImage =
+                    (Bitmap) Bitmap.FromFile(@"D:\wallpapers\july-10-photographydock-nocal-1920x1200.tif"))
+            {
+                using (var memStream = new MemoryStream())
+                {
+                    absentRectangleImage.Save(memStream, ImageFormat.Jpeg);
+                    var bytes = memStream.ToArray();
+                    return base.File(bytes, "image/"+format);
+                }
+            }
+        }
+
+
+    }
+}
