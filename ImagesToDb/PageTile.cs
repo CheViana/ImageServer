51245b5b165e98a2af91bb871b1138a4f2d4a1d6
CheViana <raduga_jenya@yahoo.com>
2014-05-15 01:03:52 +0300

    some missed files

---
+++ ImagesToDb/PageTile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagesToDb
+{
+    public class PageTile
+    {
+        [Column(Order = 0), Key]
+        public int BookId { get; set; }
+        [Column(Order = 1), Key]
+        public int PageId { get; set; }
+        public int Heigth {get;set;}
+        public int Width {get;set;}
+        public int XOffset {get;set;}
+        public int YOffset {get;set;}
+        public byte[] TileContent { get; set; }
+    }
+
+    public class TilesContext : DbContext 
+    {
+        public DbSet<PageTile> Tiles { get; set; }
+    }
+
+}
---
+++ ImagesToDb/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ImagesToDb
+{
+    public class Program
+    {
+        private static readonly Image image = Image.FromFile(@"C:\photoes\wallpapers\mar-14-hello-march-cheep-cheep-cal-1920x1080.jpg");
+
+        public static void Main(string[] args)
+        {
+            //using (var memStream = new MemoryStream())
+            //{
+            //    image.Save(memStream, ImageFormat.Jpeg);
+            //    var bytes = memStream.ToArray();
+            //    using (var context = new TilesContext())
+            //    {
+            //        var tile = new PageTile()
+            //        {
+            //            PageId = 1,
+            //            BookId = 1,
+            //            Heigth = 1080,
+            //            Width = 1920,
+            //            XOffset = 0,
+            //            YOffset = 0,
+            //            TileContent = bytes
+            //        };
+            //        context.Tiles.Add(tile);
+            //        context.SaveChanges();
+            //    }
+            //}
+            //using (var context = new TilesContext())
+            //{
+            //    var tile = context.Tiles.FirstOrDefault();
+            //    using (var memStream = new MemoryStream())
+            //    {
+            //        var bytes = tile.TileContent;
+            //        memStream.Write(bytes, 0, bytes.Length);
+            //        var image = Image.FromStream(memStream);
+            //        image.Save(@"C:\photoes\new.jpg", ImageFormat.Jpeg);
+            //    }
+            //}
+            
+        }
+    }
+}
